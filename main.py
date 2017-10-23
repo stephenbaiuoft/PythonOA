35d3fb4c6af3a3cbc8f36ef86251eaffc03a47f5
stephen bai <stephenbai91@gmail.com>
2017-10-22 23:36:07 -0400

    second

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-import processhtml as ph;
+import sys
+import processhtml as ph
 
 def main():
+    input_file = ""
+    output_file = ""
+    while len(input_file) == 0 or len(output_file) == 0:
+        if len(input_file) == 0:
+            input_file = input("please enter input filename: ")
+        if len(output_file) == 0:
+            output_file = input("please enter output filename: ")
+
+    process_file(input_file, output_file)
+
 
     pass
 
+def process_file(input_file, output_file):
+    r_dic = ph.init_regex()
+
+    try:
+        fin = open(input_file, 'rt')
+        fout = open(output_file, 'wt')
+        try:
+            for line in fin:
+                line = ph.process_line(line, r_dic)
+                fout.writelines(line)
+                print (line)
+        except:
+            print("error with ph processing")
+
+    except IOError:
+        print("failed to open " + input_file + " or " + output_file)
+        print(IOError)
+        fin.close()
+        fout.close()
 
 if __name__ == '__main__':
     main()
--- processhtml.py
+++ processhtml.py
@@ -1,30 +1,43 @@
 import re;
 
-def process_line(input, re_pattern):
+
+def process_line(input, r_dic):
     #prog = re.compile(pattern)
-    result = re_pattern.match(input)
+    for rc_pattern, cm_pattern in r_dic:
+        # updating input through all regex expressions
+        input = rc_pattern.sub(cm_pattern, input, 0 )
 
-    pass
+    return input
 
 
+# init dictionary of regex for html tags
 def init_regex():
-    rc_html = re.compile(r'(</*html[\w\d\s"\'=]*>)', re.IGNORECASE)
-    rc_head = re.compile(r'(</*head[\w\d\s"\'=]*>)', re.IGNORECASE)
-    rc_title = re.compile(r'(</*title[\w\d\s"\'=]*>)', re.IGNORECASE)
-    rc_body = re.compile(r'(</*body[\w\d\s"\'=]*>)', re.IGNORECASE)
-    rc_br = re.compile(r'(<br[\s=]*>)', re.IGNORECASE)
-    rc_h = re.compile(r'(</*h[1-7][\w\d\s"\'=]*>)', re.IGNORECASE)
-    rc_p = re.compile(r'(</*p[\w\d\s"\'=]*>)', re.IGNORECASE)
-    rc_a = re.compile(r'(</*A[\w\d\s"\'=]*>)', re.IGNORECASE)
+    rc_html = r'(</*html[\w\d\s"\'=]*>)'
+    rc_head = r'(</*head[\w\d\s"\'=]*>)'
+    rc_title = r'(</*title[\w\d\s"\'=]*>)'
+    rc_body = r'(</*body[\w\d\s"\'=]*>)'
+    rc_br = r'(<br[\s=]*>)'
+    rc_h = r'(</*h[1-7][\w\d\s"\'=]*>)'
+    rc_p = r'(</*p[\w\d\s"\'=]*>)'
+    rc_a = r'(</*A[\w\d\s"\'=]*>)'
 
-    rcm_html = r'\\color[RED]\1'
-    rcm_r_head = r'\\color[YELLOW]\1'
-    rcm_title = r'\\color[GREEN]\1'
-    rcm_r_body = r'\\color[TURQUOISE]\1'
-    rcm_br = r'\\color[PINK]\1'
-    rcm_h = r'\\color[DARKGREEN]\1'
-    rcm_p = r'\\color[DARKGRAY]\1'
-    rcm_a = r'\\color[BLUE]\1'
+    cm_html = r'\\color[RED]\1'
+    cm_head = r'\\color[YELLOW]\1'
+    cm_title = r'\\color[GREEN]\1'
+    cm_r_body = r'\\color[TURQUOISE]\1'
+    cm_br = r'\\color[PINK]\1'
+    cm_h = r'\\color[DARKGREEN]\1'
+    cm_p = r'\\color[DARKGRAY]\1'
+    cm_a = r'\\color[BLUE]\1'
 
+    r_dic = {rc_html: cm_html,
+             rc_head: cm_head,
+             rc_title: cm_title,
+             rc_body: cm_r_body,
+             rc_br: cm_br,
+             rc_h: cm_h,
+             rc_p: cm_p,
+             rc_a: cm_a
+             }
 
-    pass
+    return r_dic
